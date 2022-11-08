738f83e4215864e5783c41a4ea6df6aae3f3c304
Nairi Hovhannisyan <Nairi.Hovhannisyan@microchip.com>
2022-11-08 10:24:09 +0400

    Added cmd line.

--- src/cmd/CommandLineMgr.cxx
+++ src/cmd/CommandLineMgr.cxx
@@ -1,1 +1,43 @@
 #include "cmd/CommandLineMgr.hxx"
+
+
+#include "cli/cli.h"
+#include "cli/clilocalsession.h"
+#include "cli/filehistorystorage.h"
+
+
+void Cli::Menu::
+cliLocalSessionExitHandler(std::ostream& out)
+{
+    out << "VostanEditor session closed!\n";
+    assert(0 != m_scheduler);
+    m_scheduler->Stop();
+}
+
+void Cli::Menu::
+defaultCommandHandler(std::ostream& out, const std::string& cmd)
+{
+    out << cmd << "\n";
+}
+
+Cli::Menu::
+Menu() :
+    m_cli(nullptr),
+    m_menu(nullptr),
+    m_session(nullptr),
+    m_scheduler(nullptr)
+{
+    cli::SetColor();
+    m_menu = std::make_unique<cli::Menu>("VostanEditor");
+    m_cli = new cli::Cli( std::move(m_menu),
+            std::make_unique<cli::FileHistoryStorage>(".cli"));
+    m_scheduler = new cli::LoopScheduler();
+    m_session = new cli::CliLocalTerminalSession(
+            *m_cli, *m_scheduler, std::cout, 200);
+    auto f = std::mem_fun(&Menu::cliLocalSessionExitHandler);
+    m_session->ExitAction(std::bind(f, this, std::placeholders::_1));
+    auto g = std::mem_fn(&Menu::defaultCommandHandler);
+    m_cli->DefaultCommandHandler(std::bind(g, this,
+                std::placeholders::_1, std::placeholders::_2));
+    m_scheduler->Run();
+}
